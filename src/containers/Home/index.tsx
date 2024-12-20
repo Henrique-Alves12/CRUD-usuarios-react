dc69b1aca0b9bc8184c4d80b0ca017ea6dffa0a9
Luciano Ribeiro <luciano.ribeiro.melo02@gmail.com>
2024-12-19 22:01:18 -0300

    criando primeiro componente

--- src/containers/Home/index.tsx
+++ src/containers/Home/index.tsx
@@ -1,8 +1,44 @@
+import { useState } from "react"
+
+interface EntradaTextoProps {
+     legenda: string
+     marcador: string
+     tipoDeEntrada?: "password" | "text"
+     setarNovoValor: (valor: string) => void
+}
+
+const EntradaTexto = ({legenda, marcador, tipoDeEntrada="text", setarNovoValor}: EntradaTextoProps)=> {
+
+     const aoMudar = (evento: React.ChangeEvent<HTMLInputElement>) => {
+          const novoValor = evento.target.value;
+          setarNovoValor(novoValor);
+     };
+
+     return (
+          <div className="flex justify-between items-center">
+               <div>{legenda}</div>
+               <input type={tipoDeEntrada} placeholder={marcador} onChange={aoMudar} />
+          </div>
+     )
+}
+
 
 function Home () {
+     const titulo: string = "Projeto base"
+
+     const [nome, setNome] = useState<string>("")
+     const [email, setEmail] = useState<string>("")
+
+     
      return (
-        <div>
-             <h1 className="text-xl, font-bold">Hello Tailwind</h1>
+        <div className="flex flex-col gap-4">
+          <h1 className="text-xl font-bold">{titulo}</h1>
+
+          <EntradaTexto legenda={"nome"} marcador={"Informe seu Nome"} setarNovoValor={setNome}/>
+          <EntradaTexto legenda={"email"} marcador={"Informe seu Email"} setarNovoValor={setEmail}/>
+              
+          <button onClick={()=> alert(`Nome: ${nome} - Email: ${email}`)}>Enviar</button>
+
         </div>
      )
 
